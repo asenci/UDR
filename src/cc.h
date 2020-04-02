3517204bb1a44111d6296fefc877b813d9d6a2f8
Andre Sencioles <asenci@gmail.com>
2020-04-02 15:38:06 +1300

    Change libudt include path

--- src/cc.h
+++ src/cc.h
@@ -3,21 +3,21 @@ Copyright 2012 Laboratory for Advanced Computing at the University of Chicago
 
 This file is part of UDR.
 
-Licensed under the Apache License, Version 2.0 (the "License"); 
-you may not use this file except in compliance with the License. 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing,
- software distributed under the License is distributed on an "AS IS" BASIS, 
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-See the License for the specific language governing permissions 
+ software distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions
 and limitations under the License.
 *****************************************************************************/
 
 #include <udt.h>
-#include <ccc.h>
+#include <udt/ccc.h>
 
 class CTCP: public CCC
 {
@@ -106,8 +106,8 @@ class CUDPBlast: public CCC
 public:
    CUDPBlast()
    {
-      m_dPktSndPeriod = 1000000; 
-      m_dCWndSize = 83333.0; 
+      m_dPktSndPeriod = 1000000;
+      m_dCWndSize = 83333.0;
    }
 
 public:
